e357a7482d2f33c0c78c0c5ff298546479f92ee0
Anna Kioko <annakioko00@gmail.com>
2024-01-09 14:42:17 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const divide = () => 2000 / 100;
+
+
+const square = (num) => num * num;
+
+
+const add = (a, b) => a + b;
+
+
+console.log(divide()); 
+console.log(square(5));
+console.log(add(3, 7)); 
